3c0863ceec6bf4936e08443660305869238dc947
Jotis1 <74616896+Jotis1@users.noreply.github.com>
2021-02-15 18:39:46 +0100

    Add files via upload

--- comandos/meme.js
+++ comandos/meme.js
@@ -1,20 +1,42 @@
 const Discord = require("discord.js")
-const randomPuppy = require("random-puppy")
+const randomPuppy = require('random-puppy');
 
 module.exports = {
     nombre: "meme",
     alias: [],
     run: async (client, message, args) => {
-        const meme = ["Meme"]
-        const random = meme[Math.floor(Math.random() * meme.length)]          
-
-        const img = await randomPuppy(random)
-        const embed = new Discord.MessageEmbed()
-        .setColor("RANDOM")
-        .setFooter(message.guild.name, message.guild.iconURL())
-        .setImage(img) 
-        .setTitle("**Meme:**")
-        .setDescription("**Categoría: **" + random)
-        message.channel.send(embed) 
-    }
+        let reddit = [
+            "meme",
+            "animemes",
+            "MemesOfAnime",
+            "animememes",
+            "AnimeFunny",
+            "dankmemes",
+            "dankmeme",
+            "wholesomememes",
+            "MemeEconomy",
+            "techsupportanimals",
+            "meirl",
+            "me_irl",
+            "2meirl4meirl",
+            "AdviceAnimals"
+          ]
+        
+          let subreddit = reddit[Math.floor(Math.random() * reddit.length)];
+        
+          message.channel.startTyping();
+          
+          randomPuppy(subreddit).then(async url => {
+            const embed = new Discord.MessageEmbed()
+            .setTitle("AniMeme")
+            .setDescription(`Categoría: ${subreddit}`)
+            .setImage(url)
+            .setColor("YELLOW")
+            .setFooter("🎌| Hololive Addicts |🎌", "https://cdn.discordapp.com/attachments/806615412271611934/810927917013794846/Mori_Calliope_2.jpg")
+            await message.channel.send({embed
+            }).then(() => message.channel.stopTyping());
+          }).catch(err => console.error(err));
+        
+        }
+    
 }
